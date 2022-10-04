fb9007a3bbebb119ddc7fa6eb38ff0557cb55371
Harry Lumb <109952694+HarryLumb@users.noreply.github.com>
2022-10-04 15:35:13 +0100

    Add files via upload

---
+++ M1-D6 Javascript/index.js
@@ -0,0 +1,16 @@
+let firstName = ' Harry'
+let middleName = ' Oliver'
+let lastName = ' Lumb'
+let fullName = firstName + middleName + lastName
+let currentYear = 2022
+let age = 28
+
+
+console.log('Hello, my name is' + fullName);
+
+
+let birthYear = currentYear - age
+
+console.log(birthYear)
+
+
